d55fed54b74572206a13411987482f2460a2d169
caoyinghang <1210732802@qq.com>
2017-07-10 16:55:11 +0800

    test

---
+++ app/src/main/java/com/example/caoyinghang/cja2y/base/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.example.caoyinghang.cja2y.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by caoyinghang on 2017/7/10.
+ */
+
+public class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/caoyinghang/cja2y/scheme/SchemeActivity.java
@@ -0,0 +1,18 @@
+package com.example.caoyinghang.cja2y.scheme;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.caoyinghang.cja2y.base.BaseActivity;
+
+/**
+ * Created by caoyinghang on 2017/7/10.
+ */
+
+public class SchemeActivity extends BaseActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
